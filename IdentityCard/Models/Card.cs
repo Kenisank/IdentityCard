9ede5ebec9d8b4e4430e9d2d6e0420403256c775
Kenechukwu Frank <kenisank1@gmail.com>
2022-11-15 06:00:14 +0100

    completed without pix yet

--- IdentityCard/Models/Card.cs
+++ IdentityCard/Models/Card.cs
@@ -1,10 +1,20 @@
-﻿namespace IdentityCard.Models
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IdentityCard.Models
 {
     public class Card
     {
         public string EmployeeID { get; set; }
         public string EmployeeName { get; set; }
         public string Photo { get; set; }
+      //  public HttpPostedFileBased file { get; set; }
+      //  public HttpPostedFile ImageFile { get; set; }
         public string Position { get; set; }
     }
 }
